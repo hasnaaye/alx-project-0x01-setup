2586ce1ddec32d0114991a516bfb72f957f0d62b
hasnaaye <hasnaeyerrou@gmail.com>
2025-06-01 21:13:23 +0100

    Update index.ts

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -16,7 +16,10 @@ export interface PostModalProps {
   onClose: () => void;
   onSubmit: (post: PostData) => void;
 }
-
+export interface UserModalProps {
+  onClose: () => void;
+  onSubmit: (post: UserProps) => void;
+}
 export interface UserProps {
   id: number;
   name: string;
