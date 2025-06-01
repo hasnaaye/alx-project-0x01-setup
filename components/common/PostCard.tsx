1d43add0995db18972abe0aa7f357ed2408b9b95
hasnaaye <hasnaeyerrou@gmail.com>
2025-06-01 17:01:46 +0100

    Update PostCard.tsx

--- components/common/PostCard.tsx
+++ components/common/PostCard.tsx
@@ -1,1 +1,9 @@
-
+const PostCard: React.FC<PostCardProps> = ({ text, onClick }) => {
+  return (
+    <div 
+      className="px-4 py-2 bg-blue-500 text-white rounded" 
+      onClick={onClick}>
+      {text}
+    </div>
+  );
+};
