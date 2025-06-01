9541bf05bca50796a629d99630629e9b02692183
hasnaaye <hasnaeyerrou@gmail.com>
2025-06-01 18:55:43 +0100

    Update index.tsx

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -2,6 +2,7 @@ import PostCard from "@/components/common/PostCard";
 import Header from "@/components/layout/Header";
 import { PostProps } from "@/interfaces";
 
+
 const Posts: React.FC<PostProps[]> = ({ posts }) => {
   console.log(posts)
   return (
