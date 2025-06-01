cfcd45cd4956900720ad0827dace9b7800742cc2
hasnaaye <hasnaeyerrou@gmail.com>
2025-06-01 17:57:30 +0100

    Update index.tsx

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,1 +1,14 @@
 import Header from "@/components/layout/Header";
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
+export default Users
