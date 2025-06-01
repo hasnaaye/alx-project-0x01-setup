4baf61536d47d1b1eb475e6441aee8efc5f3b746
hasnaaye <hasnaeyerrou@gmail.com>
2025-06-01 18:52:17 +0100

    Update index.tsx

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -2,15 +2,15 @@ import PostCard from "@/components/common/UserCard";
 import Header from "@/components/layout/Header";
 import { PostProps } from "@/interfaces";
 
-const Users: React.FC<UserProps[]> = ({ posts }) => {
+const Users: React.FC<UsersPageProps[]> = ({ users }) => {
   console.log(users)
   return (
     <div className="flex flex-col h-screen">
       <Header />
       <main className="p-4">
         <div className="flex justify-between">
-        <h1 className=" text-2xl font-semibold">Post Content</h1>
-        <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
+        <h1 className=" text-2xl font-semibold">User Content</h1>
+        <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add User</button>
         </div>
         <div className="grid grid-cols-3 gap-2 ">
           {
