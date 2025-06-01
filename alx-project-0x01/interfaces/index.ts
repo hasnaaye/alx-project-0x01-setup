854a34a40f8790b83f367b643fae3d5e3ae13816
hasnaaye <hasnaeyerrou@gmail.com>
2025-06-01 19:40:39 +0100

    Update index.ts

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -1,43 +1,13 @@
-export interface PostProps {
+export interface Post {
   userId: number;
   id: number;
   title: string;
   body: string;
 }
 
-export interface UserProps {
-    id:       number;
-    name:     string;
-    username: string;
-    email:    string;
-    address:  Address;
-    phone:    string;
-    website:  string;
-    company:  Company;
-}
-
-export interface Address {
-    street:  string;
-    suite:   string;
-    city:    string;
-    zipcode: string;
-    geo:     Geo;
-}
-
-export interface Geo {
-    lat: string;
-    lng: string;
-}
-
-export interface Company {
-    name:        string;
-    catchPhrase: string;
-    bs:          string;
-}
-
 export interface PostData {
   userId: number;
-  id?: number;
+  id?: number;       
   title: string;
   body: string;
 }
@@ -47,3 +17,31 @@ export interface PostModalProps {
   onSubmit: (post: PostData) => void;
 }
 
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+export interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+export interface Geo {
+  lat: string;
+  lng: string;
+}
+
+export interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
